e5dbdbce258e49d4ca38d1aaf9751a2c358c1356
srikanth19 <sriyadav019@gmail.com>
2016-05-19 15:14:18 +0530

    landscape orientation

---
+++ app/src/main/java/app/com/example/user/truefalse/Question.java
@@ -0,0 +1,30 @@
+package app.com.example.user.truefalse;
+
+/**
+ * Created by user on 14-Mar-16.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
